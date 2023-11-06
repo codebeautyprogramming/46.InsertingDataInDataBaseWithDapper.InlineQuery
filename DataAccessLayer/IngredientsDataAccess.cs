63448c4050d9da764e5ff8acdad3df05c56a674d
codebeautyprogramming <codebeautyprogramming@gmail.com>
2023-11-06 22:22:09 +0100

    Startup code for this chapter

---
+++ DataAccessLayer/IngredientsDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using DomainModel.Models;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class IngredientsDataAccess
+    {
+        public void AddIngredient(Ingredient ingredient)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["CookBookConnectionString"].ConnectionString;
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute("dbo.InsertIngredientProcedure @Name, @Weight, @KcalPer100g, @PricePer100g, @Type", ingredient);
+            }
+        }
+    }
+}
